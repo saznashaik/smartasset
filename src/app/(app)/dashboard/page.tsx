974f37ab9efec151006f40d3b3bec0f685d453c5
Shaik Sazna <saznashaik@gmail.com>
2026-01-05 14:27:21 +0000

    make the content are larger that we have to scroll for 20times to get to

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -64,7 +64,7 @@ export default function DashboardPage() {
                     </Card>
                 ))}
             </div>
-            <div className="flex-1 rounded-lg overflow-hidden">
+            <div className="flex-1 rounded-lg overflow-hidden h-[2000vh]">
                 <iframe
                     width="100%"
                     height="100%"
