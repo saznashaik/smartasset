2b9176cff86784a5a70066bcb8d20fedd4a954f0
Shaik Sazna <saznashaik@gmail.com>
2026-01-05 14:19:51 +0000

    increase the conent area size moree aleast we have to scroll 4times like

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -64,7 +64,7 @@ export default function DashboardPage() {
                     </Card>
                 ))}
             </div>
-            <div className="flex-1 rounded-lg overflow-hidden h-[150vh]">
+            <div className="flex-1 rounded-lg overflow-hidden h-[400vh]">
                 <iframe
                     width="100%"
                     height="100%"
