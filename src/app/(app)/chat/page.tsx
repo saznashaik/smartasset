152d60c5befc346801761fce650b07772361ab67
Shaik Sazna <saznashaik@gmail.com>
2026-01-05 13:44:41 +0000

    i dont want a seperat container in chat but all the page should be quick

--- src/app/(app)/chat/page.tsx
+++ src/app/(app)/chat/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
-
 export default function ChatPage() {
     return (
-        <Card className="h-[calc(100vh-10rem)] flex flex-col">
-            <CardContent className="flex-1 overflow-hidden p-0">
-                <iframe
-                    src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/430248170338/chatagents/15dcd6d3-12e0-4bcc-b0bb-999a4a3de9c3?directory_alias=Keerthisri"
-                    style={{ border: 'none', width: '100%', height: '100%' }}>
-                </iframe>
-            </CardContent>
-        </Card>
+        <div className="h-full w-full -m-4 sm:-m-6">
+            <iframe
+                src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/430248170338/chatagents/15dcd6d3-12e0-4bcc-b0bb-999a4a3de9c3?directory_alias=Keerthisri"
+                style={{ border: 'none', width: '100%', height: '100%' }}>
+            </iframe>
+        </div>
     );
 }
