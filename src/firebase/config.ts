e3f693b51032a2f432d26f5dcf0199f7d9312ff4
Shaik Sazna <saznashaik@gmail.com>
2026-01-21 05:08:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-14622554-d9b90",
+  "appId": "1:222459763046:web:81ca81941f6783190310a7",
+  "apiKey": "AIzaSyBTZrBl-oqN7HuvLtErtkClLbFAueCxtK4",
+  "authDomain": "studio-14622554-d9b90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "222459763046"
+};
